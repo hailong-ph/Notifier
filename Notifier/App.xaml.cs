e6d91e931a0c4bcf66a3e00b406076a08d89a8e3
Yan, Hai long <HaiLong.Yan@philips.com>
2025-12-03 13:23:55 +0800

    init wpf

---
+++ Notifier/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Notifier
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
